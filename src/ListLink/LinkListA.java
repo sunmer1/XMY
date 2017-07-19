57a85505cd0e40e0f9b8bbf14d20ebbec2c951a3
sunmer <xiamy15120@163.com>
2017-07-19 18:01:32 +0800

    0719

--- src/ListLink/LinkListA.java
+++ src/ListLink/LinkListA.java
@@ -4,5 +4,38 @@ public class LinkListA {
 	public Node first;
 	public int tmp;
 	public int len=0;
+	
+	public LinkListA()
+	{
+		this.first=null;
+	}
+	
+	public void addFirstNode(Teacher teacher)
+	{
+		Node node=new Node(teacher);
+		node.next=first;
+		first=node;
+		len++;
+	}
+	
+	public Node deleteFirstNode()
+	{
+		Node node=null;
+		if(first==null)
+		{
+			System.out.println("É¾³ýÊ§°Ü£¬¸ÃÁ´±íÎª¿Õ");
+		}
+		else
+		{
+			node=first;
+			first=node.next;
+		}
+		return node;
+	}
+	
+	public void addNode(int index,Teacher teacher)
+	{
+		
+	}
 
 }
